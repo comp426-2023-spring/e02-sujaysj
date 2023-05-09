071eea2e30f2c771b07946820ede7eb3a068a0cf
sujaysj <sujaysj@gmail.com>
2023-05-09 13:20:06 -0400

    beginning scripts work

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,38 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+const endpoint = "app/";
+const url = document.baseURI+endpoint;
+
+// utility function
+function clearRadio(name) {
+    var radio = document.getElementsByName(name);
+    for (var i = 0; i < radio.length; i++) {
+        radio[i].checked = false;
+    }
+}
+
+var opponent;
+var game;
+
+var output_types = document.getElementsByName('outputtype');
+for (let i = 0; i < output_types.length; i++) {
+    output_types[i].addEventListener("change", function(event) {
+        opponent = event.target.value;
+        document.getElementById('Game Selection').style.display = 'block';
+    })
+}
+
+var gameselect = document.getElementsByName('gameselect');
+for (let i = 0; i < gameselect.length; i++) {
+    gameselect[i].addEventListener("change", function(event) {
+        game = event.target.value;
+        if (game = 'rps' && ) {
+            
+        }
+        if (game = 'rpsls') {
+
+        }
+    })
+}
+
